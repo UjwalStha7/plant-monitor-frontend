cad8314236b92ea6555cb6cbb4a95312923a8f76
Ujwal Shrestha <sthaujwal07@gmail.com>
2026-01-14 08:50:08 +0545

    Create index.ts

---
+++ src/hooks/index.ts
@@ -0,0 +1,15 @@
+// src/hooks/index.ts
+
+// Export all functions from useSensorData
+export { 
+  useSensorData, 
+  useCurrentSensorData, 
+  useSensorHistory 
+} from './useSensorData';
+
+// Export the type
+export type { UseSensorDataResult } from './useSensorData';
+
+// If you have other hooks, export them here as well
+// Example:
+// export { useOtherHook } from './useOtherHook';
